ec80c0fe36178b09500109b71df28b25fd80d597
leeeez <leeeez@leeeezui-MacBookPro.local>
2026-08-19 19:30:30 +0900

    fix : 홈 소셜 링크 Threads 계정 URL 수정

--- src/components/home/scenes/NoticeScene.tsx
+++ src/components/home/scenes/NoticeScene.tsx
@@ -25,7 +25,7 @@ const SOCIAL_LINKS = [
   },
   {
     name: "Threads",
-    url: "https://www.threads.com/@wouldyouescape.official",
+    url: "https://www.threads.com/@wouldyouescape",
     icon: (
       <svg viewBox="0 0 24 24" className="h-6 w-6 fill-current sm:h-8 sm:w-8">
         <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm0-14c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6z" />
